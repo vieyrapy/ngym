700ef21894ab7bc5af7f89178bfb23497d33302d
Hector Vieyra <latinotanato@gmail.com>
2026-02-19 19:41:18 -0300

    Add files via upload

--- ngym.py
+++ ngym.py
@@ -215,9 +215,9 @@ st.markdown("""
 # 8. CLASES EXCLUSIVAS
 st.markdown("<div class='section-title'>Clases Grupales <span class='gradient-text'>Incluidas</span></div>", unsafe_allow_html=True)
 st.markdown("""
-<div style="text-align: center; margin-bottom: 1rem; color: #94a3b8; font-weight: 600; font-size: 1rem;">
+<p style="text-align: center; margin-bottom: 1rem; color: #94a3b8; font-weight: 600; font-size: 1rem;">
 Únete a las clases grupales exclusivos para socios activos.
-</div>
+</p>
 """, unsafe_allow_html=True)
 
 st.markdown("""
